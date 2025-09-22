308b23148c425892539030967865ecdc177b8550
Yannik Tausch <yannik.tausch@quantco.com>
2025-09-22 14:57:05 +0200

    add comment about package naming

--- .github/scripts/sync-deps.sh
+++ .github/scripts/sync-deps.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 # Bash Script to sync explicit dependencies from pixi.lock into requirements.txt
+# Note: If you add any dependencies which do have a different name on PyPI,
+# you need to adjust this script to address that.
 set -euo pipefail
 
 echo "# please sync the dependencies with pyproject.toml and pixi.toml" > requirements.txt
