dd6452e01258320ac096eface08a7247c7b58407
Yannik Tausch <yannik.tausch@quantco.com>
2025-09-22 14:58:11 +0200

    Update .github/scripts/sync-deps.sh
    Co-authored-by: Pavel Zwerschke <pavelzw@gmail.com>

--- .github/scripts/sync-deps.sh
+++ .github/scripts/sync-deps.sh
@@ -6,4 +6,4 @@
 set -euo pipefail
 
 echo "# please sync the dependencies with pyproject.toml and pixi.toml" > requirements.txt
-pixi list --explicit --json | jq -r '.[] | select(.name != "python") | "\(.name)==\(.version)"' >> requirements.txt
+pixi list -e default --explicit --json | jq -r '.[] | select(.name != "python") | "\(.name)==\(.version)"' >> requirements.txt
