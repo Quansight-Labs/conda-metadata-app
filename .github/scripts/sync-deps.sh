621b21235e8dca22388b7c4332ed3e2e39b09930
Yannik Tausch <yannik.tausch@quantco.com>
2025-09-22 15:29:12 +0200

    add CI check for pixi.toml / pyproject.toml sync

--- .github/scripts/sync-deps.sh
+++ .github/scripts/sync-deps.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# Bash Script to sync explicit dependencies from pixi.lock into requirements.txt
+# Sync explicit dependencies from pixi.lock into requirements.txt
 # Note: If you add any dependencies which do have a different name on PyPI,
 # you need to adjust this script to address that.
 set -euo pipefail
 
-echo "# please sync the dependencies with pyproject.toml and pixi.toml" > requirements.txt
+echo "# this file is auto-generated, please make changes in pixi.toml instead" > requirements.txt
 pixi list -e default --explicit --json | jq -r '.[] | select(.name != "python") | "\(.name)==\(.version)"' >> requirements.txt
