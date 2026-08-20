71ea4506c1b293d93180047269118a7056cb8c83
Yannik Tausch <yannik.tausch@quantco.com>
2026-08-20 11:32:48 +0200

    ci: keep test dependencies out of production

--- .github/scripts/sync-deps.sh
+++ .github/scripts/sync-deps.sh
@@ -6,4 +6,4 @@
 set -euo pipefail
 
 echo "# this file is auto-generated, please make changes in pixi.toml instead" > requirements.txt
-pixi list -e default --explicit --json | jq -r '.[] | select(.name != "python") | "\(.name)==\(.version)"' >> requirements.txt
+pixi list -e prod --explicit --json | jq -r '.[] | select(.name != "python") | "\(.name)==\(.version)"' >> requirements.txt
